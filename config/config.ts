ee88ae3bb06ad7f14d62b8e1099d2da36c00999e
danielnifty <danielobichere@gmail.com>
2024-08-20 21:35:33 +0100

    fixing proccess.env

--- config/config.ts
+++ config/config.ts
@@ -26,7 +26,7 @@ export default class TypeOrmConfig {
     return {
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt( process.env.PORT),
+      port: parseInt( process.env.DB_PORT),
 
       username:  process.env.DB_USERNAME,
       password:  process.env.DB_PASSWORD,
