f0bbf6d265a4f48d75702fa9b271f5154fbcd640
danielnifty <danielobichere@gmail.com>
2024-08-21 02:48:33 +0100

    enabling cors

--- src/mail/service/mail.service.ts
+++ src/mail/service/mail.service.ts
@@ -10,16 +10,13 @@ export class MailService {
 
    async sendMail(book:Book){
 
-    const url = `${this.configService.get(
-        'SITE_EMAIL',
-      )}/auth/forgot-password/reset?`;
-  
+
       await this.mailerService.sendMail({
         to: this.configService.get("SITE_EMAIL"),
         subject:  `New Booking By ${book.fullname}`,
         template: './booking',
         context: {
-          url,
+         
           name: book.fullname ,
           email: book.email,
         },
