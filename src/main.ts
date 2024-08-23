851201cdf0c12ee8ed242b85d35613487d5bd36c
danielnifty <danielobichere@gmail.com>
2024-08-23 20:41:53 +0100

    cors correction

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('v1');
-  // somewhere in your initialization file
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          imgSrc: [`'self'`, 'data:', 'apollo-server-landing-page.cdn.apollographql.com'],
-          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
-          manifestSrc: [`'self'`, 'apollo-server-landing-page.cdn.apollographql.com'],
-          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
-        },
-      }
-    }), 
-  );
-  app.enableCors({origin:['http://127.0.0.1:5502','http://localhost:5173','http://127.0.0.1:5173/','https://vitfit-v2.netlify.app/']});
+
+  app.enableCors({origin:['http://127.0.0.1:5502','http://localhost:5173','http://127.0.0.1:5173/','https://vitfit-v2.netlify.app/','http://vitfit-v2.netlify.app/']});
   await app.listen(8080);
 }
 bootstrap();
