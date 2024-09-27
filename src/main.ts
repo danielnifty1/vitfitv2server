049e389bcd1bfaf1ef4a3aeba069d87a84e6a981
danielnifty1 <danielobichere@gmail.com>
2024-09-27 19:27:04 +0100

    mail server

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,18 @@ async function bootstrap() {
 
   app.setGlobalPrefix('v1');
 
-  app.enableCors({origin: ["https://vitfit-admin.netlify.app","http://127.0.0.1:5503","http://localhost","http://127.0.0.1:5501","http://127.0.0.1:5500","http://localhost:5174","http://localhost:5173","https://fitserver-else.onrender.com","https://fitvit.netlify.app","https://vitfit.netlify.app","https://vitfitcoaching.com"]});
+  app.enableCors({origin: ["https://vitfit-admin.netlify.app","http://127.0.0.1:5503",'http://localhost:5173',"https://fitserver-else.onrender.com","https://vitfit-v2.netlify.app","https://vitfitcoaching.com"]});
+
+
+
+
+  // origin:[ 'https://localhost:5173','http://localhost:5173','https://vitfit-v2.netlify.app','http://vitfit-v2.netlify.app','https://vitfitcoaching.com/','http://vitfitcoaching.com/']
+
+
+
+
+
+
 
 
   // app.use(
